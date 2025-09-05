7e70c9c07b43f275d9e45c804e74071e81153c6e
Antoine Delbos <aadelbos@gmail.com>
2025-09-05 19:21:16 +0200

    add correctif for output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   integrations: [tailwind()],
 });
