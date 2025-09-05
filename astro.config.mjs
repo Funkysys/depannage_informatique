1908c4550364855e443e814cc4e082ac80302da3
Antoine Delbos <aadelbos@gmail.com>
2025-09-05 19:18:46 +0200

    add email and phone verification for contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: 'hybrid',
+  output: "hybrid",
   integrations: [tailwind()],
 });
