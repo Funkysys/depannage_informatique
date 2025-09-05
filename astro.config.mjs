dc6711d472f6324ee2a142321faded16a60a3e00
Antoine Delbos <aadelbos@gmail.com>
2025-09-05 19:23:32 +0200

    add correctif for output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/server";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [tailwind()],
 });
