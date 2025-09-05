a99c58b5668c49f9d2e004232530898fa41ba0bf
Antoine Delbos <aadelbos@gmail.com>
2025-09-06 01:18:52 +0200

    change pricing

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,10 +4,10 @@ export type Pricing = {
 };
 
 export const pricingTable: Pricing[] = [
-  { service: "Diagnostic", price: "30€ (offert en cas de prestation)" },
-  { service: "Nettoyage", price: "30-50€/heure selon modèle" },
-  { service: "Optimisation", price: "40€/heure" },
-  { service: "Installation", price: "40€/heure" },
-  { service: "Réparation", price: "50€/heure" },
+  { service: "Diagnostic", price: "45€ (offert en cas de prestation)" },
+  { service: "Nettoyage", price: "45-90€/heure selon modèle" },
+  { service: "Optimisation", price: "90€/heure" },
+  { service: "Installation", price: "90€/heure" },
+  { service: "Réparation", price: "100€/heure" },
   { service: "Développement", price: "350€/jour" },
 ];
