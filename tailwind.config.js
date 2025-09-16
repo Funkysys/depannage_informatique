f2216da5808efc74e54179dd5305dc82ac0b62fd
Antoine Delbos <aadelbos@gmail.com>
2025-09-16 02:04:50 +0200

    upgrade layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-bg": "#0D1520",
+        "custom-border": "#2870BD",
+        "custom-nav": "#111927",
+        "custom-btn": "#003362",
+        "custom-font": "#C2E6FF",
+      },
+    },
   },
   plugins: [],
 };
